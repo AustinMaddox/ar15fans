c0a29d424960c55677181d2b6ffc740b722130e7
austin881 <austin@maddoxbox.com>
2013-01-26 22:20:16 -0700

    Navigation bar clings to the top of the window

--- styles/style_ar15fans/template/js/overall.js
+++ styles/style_ar15fans/template/js/overall.js
@@ -9,6 +9,15 @@ var Overall = Overall || {};
 		$('.kill-click').click(function() {
 			return false;
 		});
-	};
+		
+		// Navigation bar clings to the top of the window 
+		var $window = $(window);
+		var $e = $('#nav');
+		var eTop = $e.offset().top;
+		$window.scroll(function() {
+			$e.toggleClass('cling', $window.scrollTop() > eTop);
+		});
 
+	};
+	
 })(jQuery);
